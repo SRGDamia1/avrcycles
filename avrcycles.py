bfffbd0edac924cededb66eb85782f8646f397c4
Sara Damiano <sdamiano@stroudcenter.org>
2024-07-31 18:09:45 -0400

    Add recursive symbol analysis
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- avrcycles.py
+++ avrcycles.py
@@ -18,7 +18,10 @@ Description:
 Usage:
     Output a .avra with avr-objdump.
     Example:
+    For older AVR toolkits, with avr-objdump
     $ avr-objdump -h -S build/vis-spi-out.elf > build/vis-spi-out.avra
+    For newer AVR toolkits, with objdump
+    $ objdump -h -S -m avr5 -d firmware.elf  > program.avra
 
     Grab a snippet to parse and put it in a new
     file. Run this script on the snippet.
@@ -47,7 +50,10 @@ About:
 """
 
 import argparse
+import copy
+import json
 from pathlib import Path
+import re
 
 # Create a dictionary of AVR instructions to look
 # up the number of clock cycles an instruction
@@ -204,28 +210,32 @@ def print_every_line(fobj):
     """Print every line in the file.
     Report number of lines.
     """
-    lines=[line for line in fobj]
-    [print(line,end='') for line in lines]
+    lines = [line for line in fobj]
+    [print(line, end="") for line in lines]
     print(f"Total lines in file: {len(lines)}")
+
+
 def _is_not_blank(line):
     """Return true if `line` is not blank."""
-    return len(line.split())>0
+    return len(line.split()) > 0
+
+
 def _is_blank(line):
     """Return true if `line` is blank."""
-    return len(line.split())==0
+    return len(line.split()) == 0
+
+
 def print_every_line_except_blanks(fobj):
     """Print every line in the file, but skip
     blank lines.
     Report number of lines.
     """
     # lines=[line.split() for line in fobj if len(line.split())>0]
-    lines=[line.split() for line in fobj if _is_not_blank(line)]
+    lines = [line.split() for line in fobj if _is_not_blank(line)]
     [print(line) for line in lines]
-    print(
-        "Total number of lines in file, "
-        "skipping blank lines: "
-        f"{len(lines)}"
-        )
+    print("Total number of lines in file, skipping blank lines: " f"{len(lines)}")
+
+
 def _is_asm(line):
     """Return true if input `line` is a line of
     AVR assembly code.
@@ -238,28 +248,40 @@ def _is_asm(line):
         ' 1b6:	ff cf 	rjmp	.-2  	;'
     """
     # Discard blank lines
+    # print(line)
     if _is_blank(line):
+        # print("BLANK!!")
+        return False
+    # Make sure we have at least 3 words separated by tabs
+    n_words = len(line.split("\t"))
+    len_each_word = [len(word) > 0 for word in line.split("\t")]
+    all_non_blank = all(len(word) > 0 for word in line.split("\t"))
+    line_has_at_least_3_words = n_words > 2 and all_non_blank
+    # print(f"Tab sep words: {n_words}, {len_each_word}, {all_non_blank}!!")
+    if not line_has_at_least_3_words:
+        # print(f"Not three words: {n_words}, {len_each_word}, {all_non_blank}!!")
         return False
     # Check first word
-    word1=line.split()[0]
-    word1_last_char=word1[-1]
-    line_has_at_least_3_words=len(line.split())>2
+    word1 = line.split("\t")[0].strip()
+    word1_last_char = word1[-1]
+    # print(f"Word1: '{word1}', last char: '{word1_last_char}'")
+    # if word1[0:-1].isalnum() and word1_last_char == ":" and line_has_at_least_3_words:
+    #     print("Yup, that's assembly!")
     return (
-        word1[0:-1].isalnum()
-        and word1_last_char is ':'
-        and line_has_at_least_3_words
-        )
+        word1[0:-1].isalnum() and word1_last_char == ":" and line_has_at_least_3_words
+    )
+
+
 def print_only_asm_lines(fobj):
     """Print only assembly lines of code from the
     file.
     Report number of lines.
     """
-    lines=[line for line in fobj if _is_asm(line)]
-    [print(line.split('\t')) for line in lines]
-    print(
-        "Total number of assembly lines in file: "
-        f"{len(lines)}"
-        )
+    lines = [line for line in fobj if _is_asm(line)]
+    [print(line.split("\t")) for line in lines]
+    print(f"Total number of assembly lines in file: {len(lines)}")
+
+
 def _instruction(asm_line):
     """Return the instruction found in input
     `asm_line`, a line of AVR Assembly code.
@@ -269,7 +291,102 @@ def _instruction(asm_line):
     """
     # Lines are tab-separated.
     # Instruction is always the 3rd word.
-    return asm_line.split('\t')[2].strip()
+    return asm_line.split("\t")[2].strip()
+
+
+def parse_instruction(asm_line):
+    """Return the instruction found in input
+    `asm_line`, a line of AVR Assembly code.
+    Example:
+        ' 1b6:	ff cf 	rjmp	.-2  	;'
+    """
+
+    # Lines are tab-separated.
+    parsed_instr = {"address": asm_line.split("\t")[0].strip()}
+    parsed_instr["raw_instruction"] = asm_line.split("\t")[2].strip()
+    if parsed_instr["raw_instruction"] in cycles.keys():
+        parsed_instr["raw_cycles"] = cycles[parsed_instr["raw_instruction"]]
+    else:
+        print(f"Got bad instruction: '{parsed_instr['raw_instruction']}'")
+        parsed_instr["raw_cycles"] = 0
+
+    if parsed_instr["raw_instruction"] == "call":
+        if asm_line.split("\t")[3].strip() == "0":
+            parsed_instr["call_stack"] = "0x0"
+        else:
+            parsed_instr["call_stack"] = asm_line.split("\t")[3].strip()
+
+    # Instruction is always the 3rd word.
+    return parsed_instr
+
+
+def parse_fxns(fobj):
+    parsed_fxns = {}
+
+    total_fxn_cycles = 0
+    fxn_sub_calls = []
+    got_start_address = False
+
+    for line in fobj:
+        # 0000031e <digitalRead>:
+        match_symbol = re.search(r"(?P<address>[\da-f]{8}) <(?P<symbol>.*?)>:", line)
+        if match_symbol is not None:
+            if got_start_address == True:
+                # output the results from the last symbol
+                parsed_fxn = {
+                    "start_address": f"0x{parsed_fxn_start}",
+                    "end_address": f"0x{max_address}",
+                    "raw_cycles": total_fxn_cycles,
+                    "sub_calls": copy.deepcopy(fxn_sub_calls),
+                    "has_sub_calls": len(fxn_sub_calls) > 0,
+                    "parsed_fxn_name": parsed_fxn_name,
+                }
+                print(f"{parsed_fxn}")
+                parsed_fxns[parsed_fxn["start_address"]] = copy.deepcopy(parsed_fxn)
+
+            # start the new symbol
+            parsed_fxn_start = match_symbol.group("address")[4:8]
+            while parsed_fxn_start.startswith("0") and len(parsed_fxn_start) > 1:
+                parsed_fxn_start = parsed_fxn_start[1:]
+            parsed_fxn_name = match_symbol.group("symbol")
+            got_start_address = True
+            total_fxn_cycles = 0
+            fxn_sub_calls = []
+            print(f"Detected symbol start at {parsed_fxn_start}")
+
+        if _is_asm(line):
+            parsed_instr = parse_instruction(line)
+            total_fxn_cycles += parsed_instr["raw_cycles"]
+            max_address = parsed_instr["address"][:-1]
+            if "call_stack" in parsed_instr.keys():
+                fxn_sub_calls.append(parsed_instr["call_stack"])
+
+    # print(parsed_fxns)
+    for fxn_start, parsed_fxn in parsed_fxns.items():
+        parsed_fxns[fxn_start]["total_cycles"] = get_fxn_stack_size(
+            parsed_fxns, fxn_start
+        )
+
+    json.dump(parsed_fxns, open("dissassembly_parsed.json", "w+"), indent=2)
+    return parsed_fxns
+
+
+def get_fxn_stack_size(parsed_fxns, fxn_start_address):
+    this_fxn = parsed_fxns[fxn_start_address]
+    if this_fxn["has_sub_calls"] == False:
+        print(
+            f"Function starting at {fxn_start_address} is a simple function with {this_fxn['raw_cycles']} cycles"
+        )
+        return this_fxn["raw_cycles"]
+    print(
+        f"Function starting at {fxn_start_address} has sub-calls to {this_fxn['sub_calls']}"
+    )
+    return this_fxn["raw_cycles"] + sum(
+        get_fxn_stack_size(parsed_fxns, sub_address)
+        for sub_address in this_fxn["sub_calls"]
+    )
+
+
 def list_of_instructions_in_file(fobj):
     """Return a list of assembly instructions parsed from
     the input `fobj` file object. Duplicates are included.
@@ -280,9 +397,13 @@ def list_of_instructions_in_file(fobj):
     Append it to the list.
     """
     # Get all the assembly lines
-    lines=[line for line in fobj if _is_asm(line)]
+    lines = [line for line in fobj if _is_asm(line)]
+    print(f"Total number of assembly lines in file: {len(lines)}")
+
     # return the list of instructions
     return [_instruction(line) for line in lines]
+
+
 def print_instruction_from_every_line(fobj):
     """Print the instructions from an AVR Assembly
     file. Ignore lines that are not AVR Assembly.
@@ -291,15 +412,13 @@ def print_instruction_from_every_line(fobj):
     # lines=[line for line in fobj if _is_asm(line)]
     # Pull out the instructions
     # instructions=[_instruction(line) for line in lines]
-    instructions=list_of_instructions_in_file(fobj)
-    [print(inst,end=', ') for inst in instructions]
-    print(
-        "\nTotal number of assembly instructions: "
-        f"{len(instructions)}"
-        )
+    instructions = list_of_instructions_in_file(fobj)
+    [print(inst, end=", ") for inst in instructions]
+    print("\nTotal number of assembly instructions: " f"{len(instructions)}")
+
+
 def Parse_avra(filepath):
-    """Docstring
-    """
+    """Docstring"""
     # Open file
     p = Path(filepath)
     with p.open() as f:
@@ -307,15 +426,21 @@ def Parse_avra(filepath):
         # print_every_line_except_blanks(f)
         # print_only_asm_lines(f)
         # print_instruction_from_every_line(f)
-        # Find each AVR instruction
-        instructions=list_of_instructions_in_file(f)
-        # Look up the number of cycles for that instruction
-        cycles_list=[cycles[instr] for instr in instructions]
-        # Total the number of cycles.
-        print(f"Total number of cycles: {sum(cycles_list)}")
-        print(f"Total number of instructions: {len(instructions)}")
+        # # Find each AVR instruction
+        # instructions = list_of_instructions_in_file(f)
+        # # Look up the number of cycles for that instruction
+        # cycles_list = [
+        #     cycles[instr] for instr in instructions if instr in cycles.keys()
+        # ]
+        # # # Total the number of cycles.
+        # print(f"Total number of cycles: {sum(cycles_list)}")
+        # print(f"Total number of instructions: {len(instructions)}")
+        parsed_fxns = parse_fxns(f)
+        print(f"Total number of instructions:")
+        print(f"Total number of symbols: {len(parsed_fxns.keys())}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Take the file path as a command line argument.
 
     # Create a parser.
@@ -323,11 +448,8 @@ if __name__ == '__main__':
 
     # Add a positional arg.
     parser.add_argument(
-        'avra',
-        metavar='Path-to-AVRA-file',
-        type=str,
-        help='Path to .avra file.'
-        )
+        "avra", metavar="Path-to-AVRA-file", type=str, help="Path to .avra file."
+    )
 
     # =====[ the critical LOC to make this a utility. ]=====
     # Parse space-separated text after `pkgscripts`.
@@ -336,10 +458,12 @@ if __name__ == '__main__':
     # Validate the *avra file path* argument.
     path = Path(args.avra)
     # Stop execution if the file is not found.
-    assert path.exists(), f'\n  File "{path.name}" not found. Check your path:\n"{path}"'
+    assert (
+        path.exists()
+    ), f'\n  File "{path.name}" not found. Check your path:\n"{path}"'
     # Stop execution if the path is not a file.
     assert path.is_file(), f'\n  Path is not a file:\n "{path}"'
     # Stop execution if the path is not an avra.
-    assert path.suffix == '.avra', f'\n  File type "{path.suffix}" is not .avra!'
+    assert path.suffix == ".avra", f'\n  File type "{path.suffix}" is not .avra!'
 
     Parse_avra(filepath=path)
